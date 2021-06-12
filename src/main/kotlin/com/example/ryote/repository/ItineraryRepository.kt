65e9fadcaf7911a66859fe97597c91871f431c8b
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-13 07:47:06 +0900

    Add variation for itinerary repo

--- src/main/kotlin/com/example/ryote/repository/ItineraryRepository.kt
+++ src/main/kotlin/com/example/ryote/repository/ItineraryRepository.kt
@@ -7,15 +7,27 @@ import org.springframework.stereotype.Repository
 @Repository
 class ItineraryRepository {
 
-  fun getLandmarks(day: Int) = listOf(
-    Landmark(100 * day + 0, "Kyoto station", "Traditional Japanese city."),
-    Landmark(100 * day + 1, "Tokyo station", "Central Japanese city."),
-    Landmark(100 * day + 2, "Jimbocho", "Famous for its variety of book stores.")
-  )
+  fun getLandmarks(day: Int) = if (day == 0) {
+    listOf(
+      Landmark(100 * day + 0, "Kyoto station", "Traditional Japanese city."),
+      Landmark(100 * day + 1, "Tokyo station", "Central Japanese city."),
+      Landmark(100 * day + 2, "Jimbocho", "Famous for its variety of book stores.")
+    )
+  } else {
+      Landmark(100 * day + 0, "Asakusa", "Sensoji temple"),
+      Landmark(100 * day + 1, "Ikebukuro", "Nothing especially"),
+      Landmark(100 * day + 2, "Shinjuku", "Big city")
+  }
   
-  fun getTransportations(day: Int) = listOf(
-    Transportation(100 * day + 0, "Shikansen", "Tokaido"),
-    Transportation(100 * day + 1, "Yamanote Line", "Kanjosen"),
-  )
-
+  fun getTransportations(day: Int) = if (day == 0) {
+      listOf(
+        Transportation(100 * day + 0, "Shikansen", "Tokaido"),
+        Transportation(100 * day + 1, "Yamanote Line", "Kanjosen"),
+      )
+    else {
+      listOf(
+        Transportation(100 * day + 0, "Yamanote Line", "Kanjosen"),
+        Transportation(100 * day + 1, "Yamanote Line", "Kanjosen"),
+      )
+    }
 }
