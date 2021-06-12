345e2390d08b6f6db8929ccc30a88751a84c1925
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-13 07:42:35 +0900

    Add CORS

--- src/main/kotlin/com/example/ryote/controller/ItineraryController.kt
+++ src/main/kotlin/com/example/ryote/controller/ItineraryController.kt
@@ -2,6 +2,7 @@ package com.example.ryote.controller
 
 import com.example.ryote.service.ItineraryService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam
 class ItineraryController(
   @Autowired val service: ItineraryService,
 ) {
-
+  @CrossOrigin(origins = ["http://localhost:3000"])
   @GetMapping("/landmark")
   fun getLandmarks(@RequestParam(value = "day") day: Int) = service.getLandmarks(day)
   
+  @CrossOrigin(origins = ["http://localhost:3000"])
   @GetMapping("/transportation")
   fun getTransportations(@RequestParam(value = "day") day: Int) = service.getTransportations(day)
     
