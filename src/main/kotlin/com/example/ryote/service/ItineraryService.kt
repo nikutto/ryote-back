c4fec02256c97393ea9e060988d465b2b44a03d3
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-13 10:33:50 +0900

    Fix service

--- src/main/kotlin/com/example/ryote/service/ItineraryService.kt
+++ src/main/kotlin/com/example/ryote/service/ItineraryService.kt
@@ -3,22 +3,24 @@ package com.example.ryote.service
 import com.example.ryote.converter.*
 import com.example.ryote.dto.LandmarkDto
 import com.example.ryote.dto.TransportationDto
-import com.example.ryote.repository.ItineraryRepository
+import com.example.ryote.repository.LandmarkRepository
+import com.example.ryote.repository.TransportationRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class ItineraryService(
-  @Autowired val repository: ItineraryRepository
+  @Autowired val landmarkRepository: LandmarkRepository,
+  @Autowired val transportationRepository: TransportationRepository,
 ) {
 
-  fun getLandmarks(day: Int) = repository
-        .getLandmarks(day)
+  fun getLandmarks(day: Int) = landmarkRepository
+        .findByDay(day)
         .map { it.toDto() }
         .toList()
 
-  fun getTransportations(day: Int) = repository
-        .getTransportations(day)
+  fun getTransportations(day: Int) = transportationRepository
+        .findByDay(day)
         .map { it.toDto() }
         .toList()
 
