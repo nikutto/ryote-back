525894a169c770ee8369996fdcd9ce8f8b5d0b90
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-13 10:33:08 +0900

    Revise

--- src/main/kotlin/com/example/ryote/converter/LandmarkConverter.kt
+++ src/main/kotlin/com/example/ryote/converter/LandmarkConverter.kt
@@ -4,7 +4,7 @@ import com.example.ryote.dao.Landmark
 import com.example.ryote.dto.LandmarkDto
 
 fun Landmark.toDto() = LandmarkDto(
-    id = this.id,
+  id = (this.id % 1_000_000_007).toInt(),
     name = this.name,
     detail = this.detail
   )
--- src/main/kotlin/com/example/ryote/converter/TransportationConverter.kt
+++ src/main/kotlin/com/example/ryote/converter/TransportationConverter.kt
@@ -4,7 +4,7 @@ import com.example.ryote.dao.Transportation
 import com.example.ryote.dto.TransportationDto
 
 fun Transportation.toDto() = TransportationDto(
-    id = this.id,
+    id = (this.id % 1_000_000_007).toInt(),
     name = this.name,
     detail = this.detail
   )
