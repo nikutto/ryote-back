c86803ebee88fa159a631ea806ee78be4b67d836
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-22 12:36:02 +0900

    Add security dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,10 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
-	
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-test")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
