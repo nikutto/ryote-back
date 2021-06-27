d847aa567838c011632fb7df4947a1b74f15d711
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-27 11:00:00 +0900

    Format fix

--- src/main/kotlin/com/example/ryote/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/ryote/security/SecurityConfig.kt
@@ -2,7 +2,6 @@ package com.example.ryote.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
@@ -12,7 +11,6 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
-import org.springframework.http.HttpMethod
 
 @Configuration
 @EnableWebFluxSecurity
