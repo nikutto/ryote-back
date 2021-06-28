059ab812af4406c7bf10bcb7d49c9e74ca69ea0a
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-29 08:49:43 +0900

    Remove unnecessary dto

--- src/main/kotlin/com/example/ryote/dto/LandmarkDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ryote.dto
-
-data class LandmarkDto(
-    val id: Int = 0,
-    val day: Int,
-    val name: String,
-    val detail: String
-)
--- src/main/kotlin/com/example/ryote/dto/TransportationDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ryote.dto
-
-data class TransportationDto(
-    val id: Int,
-    val name: String,
-    val detail: String
-)
